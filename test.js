809e103ee61ff7decb7a85a31cce98782640bb1d
vividmint <1123429071@qq.com>
2016-08-01 23:50:49 +0800

    focus

---
+++ test.js
@@ -0,0 +1,11 @@
+var help = document.querySelector("#help");
+var fields = document.querySelectorAll("input");
+for (var i = 0; i < fields.length; i++) {
+    fields[i].addEventListener("focus", function(event) {
+        var text = event.target.getAttribute("data-help");
+        help.textContent = text;
+    });
+    fields[i].addEventListener("blur", function(event) {
+        help.textContent = "";
+    });
+}
